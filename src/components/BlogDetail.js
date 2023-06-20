e0d1cb12c02c6ee52523ae24569de62570355db2
Pita Badila <ritualmomma@gmail.com>
2023-06-20 17:11:15 -0400

    Adds styling for blogDetail

--- src/components/BlogDetail.js
+++ src/components/BlogDetail.js
@@ -1,6 +1,8 @@
 import { useLocation, Link } from 'react-router-dom'
+
 import exit from '../imgs/exit3.png'
 import ikigai from '../imgs/ikigai.jpg'
+
 const BlogDetail = (  ) => {
     const location = useLocation()
     const  { title, entry, date, id } = location.state
@@ -57,25 +59,33 @@ const BlogDetail = (  ) => {
     console.log(day)
     let revDate = month + " " + day + "," + year
     console.log(revDate)
-    // if (id === 6 || id === 9 || id === 10 || id === 11) {
-    //     return null
-    // }
+   console.log(id)
+   console.log(typeof(id))
+     const picSelect = (id) =>{
+        switch (id) {
+            case 6:
+                return ikigai
+                break
+        }
+     }
 
     
     return(
       
         <div className="blogDetailContainer">
-            <div className="blogDetailImgBox">
-                {/* <img clasName="blogDetailImg" src={ikigai}/> */}
-            </div>
-            <div className="titleBox">
-                <h1 className="detailTitle"> {title} </h1>
-                <h3 className="detailDate"> {revDate} </h3> 
-            </div>
-            <p className="detailEntry"> {entry} </p>
-            <div className="exitBox">
-                <Link to = '/' ><img className='exitDetail' src = {exit} /></Link>
-                <Link to ={`/updateblog/${id}`}><button className="updateBtn">Update</button></Link>
+            <div className="detailHolder">
+                <div className="titleBox">
+                    <h1 className="detailTitle"> {title} </h1>
+                    <h3 className="detailDate"> {revDate} </h3> 
+                </div>
+                <div className="blogDetailImgBox">
+                    <img className="blogDetailImg" src={picSelect(id)}/> 
+                </div>
+                <p className="detailEntry"> {entry} </p>
+                <div className="exitBox">
+                    <Link to = '/' ><img className='exitDetail' src = {exit} /></Link>
+                    <Link to ={`/updateblog/${id}`}><button className="updateBtn">Update</button></Link>
+                </div>
             </div>
         </div>
         
